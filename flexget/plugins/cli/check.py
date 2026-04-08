3e00a490ff17a2318b69c6f869a5c890aaca842a
tunaflsh <tunaflish.ai@gmail.com>
2026-04-09 01:42:58 +0200

    fix: deprecated warnings

--- flexget/plugins/cli/check.py
+++ flexget/plugins/cli/check.py
@@ -18,7 +18,7 @@ def before_config_load(manager):
 
 def pre_check_config(config_path):
     """Check configuration file for common mistakes that are easily detectable."""
-    with codecs.open(config_path, 'r', 'utf-8') as config_file:
+    with open(config_path, 'r', encoding='utf-8') as config_file:
         try:
             config = config_file.read()
         except UnicodeDecodeError:
@@ -147,7 +147,7 @@ def check(manager, options):
     if manager.is_daemon:
         # If we are running in a daemon, check disk config
         pre_check_config(manager.config_path)
-        with codecs.open(manager.config_path, 'r', encoding='utf-8') as config_file:
+        with open(manager.config_path, 'r', encoding='utf-8') as config_file:
             try:
                 config = yaml.safe_load(config_file)
             except yaml.error.YAMLError as e:
